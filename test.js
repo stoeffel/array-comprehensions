dad7360589258fb1dadfe3f6a87f3ec6c32169ed
Christoph Hermann <schtoeffel@gmail.com>
2015-01-12 09:53:11 +0100

    simplify tests

--- test.js
+++ test.js
@@ -4,22 +4,22 @@ var forOf = require('./forOf.js'),
 describe('for(iterable, do)', () => {
   it('should call do for each item of ther iterable', () => {
     expect(forOf([1,2,3])).to.eql([1,2,3]);
-    expect(forOf([1,2,3], x => { return x+1; })).to.eql([2,3,4]);
+    expect(forOf([1,2,3], x => x+1)).to.eql([2,3,4]);
   });
 });
 
 describe('for(iterable, condition, do)', () => {
   it('should check the condition for each item', () => {
-    expect(forOf([-1,0,1,2,3], x => { return true; }, x => { return x; })).to.eql([-1,0,1,2,3]);
-    expect(forOf([-1,0,1,2,3], x => { return false; }, x => { return x; })).to.eql([]);
-    expect(forOf([-1,0,1,2,3], x => { return x > 0; }, x => { return x; })).to.eql([1,2,3]);
+    expect(forOf([-1,0,1,2,3], x => true , x => x )).to.eql([-1,0,1,2,3]);
+    expect(forOf([-1,0,1,2,3], x => false , x => x )).to.eql([]);
+    expect(forOf([-1,0,1,2,3], x => x > 0 , x => x )).to.eql([1,2,3]);
   });
 });
 
 describe('for(iterable, iterable, do)', () => {
   it('should call do for each item and the correspondant item of the sec list', () => {
-    expect(forOf([1,2,3], [3,2,1], (x,y) => { return x*y; })).to.eql([3, 2, 1, 6, 4, 2, 9, 6, 3]);
-    expect(forOf([1,2,3], [4,3,2,1], (x,y) => { return x*y; })).to.eql([4, 3, 2, 1, 8, 6, 4, 2, 12, 9, 6, 3]);
-    expect(forOf([1,2], [3,2,1], (x,y) => { return x*y; })).to.eql([3, 2, 1, 6, 4, 2]);
+    expect(forOf([1,2,3], [3,2,1], (x,y) => x*y)).to.eql([3, 2, 1, 6, 4, 2, 9, 6, 3]);
+    expect(forOf([1,2,3], [4,3,2,1], (x,y) => x*y)).to.eql([4, 3, 2, 1, 8, 6, 4, 2, 12, 9, 6, 3]);
+    expect(forOf([1,2], [3,2,1], (x,y) => x*y)).to.eql([3, 2, 1, 6, 4, 2]);
   });
 });
